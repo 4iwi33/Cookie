45ba6625dcbecce19627a4fc5b03012d0179463d
4iwi33 <59205435+4iwi33@users.noreply.github.com>
2020-04-09 18:28:20 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 
 $pagesize = 4;
-$host = "remotemysql.com";
-$user = "c8NtAeI5eS";
-$password = "u926KRJdWp";
-$database = "c8NtAeI5eS";
+$host = "";
+$user = "";
+$password = "";
+$database = "";
